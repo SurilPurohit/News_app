b6b365599592174464b7710e18f86625e5249427
Suril <suril.p@somaiya.edu>
2019-04-23 12:13:54 +0530

    Added News API

---
+++ app/src/main/java/com/example/news/Constants.java
@@ -0,0 +1,5 @@
+package com.example.news;
+
+public class Constants {
+    public static final String URL = "https://newsapi.org/v2/everything?apiKey=18aecab4df0d462082f5801c0cf06172&q=sports";
+}
--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -29,4 +29,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
